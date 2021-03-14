bcd6533878bf409b6d0f6197c9e9464766701d9c
James Prickitt <jwprickitt@gmail.com>
2021-03-14 19:28:07 -0400

    010-pre2

--- 010.js
+++ 010.js
@@ -1,1 +1,32 @@
 // exercise 10
+// Simple validation of a username with regex
+
+// Write a simple regex to validate a username. Allowed characters are:
+
+// lowercase letters,
+// numbers,
+// underscore
+// Length should be between 4 and 16 characters (both included).
+
+function validateUsr(username) {
+  res =  //regex here/.test(username) 
+  return res
+
+}
+
+
+
+
+// Test.describe("Basic tests",_=>{
+// Test.assertEquals(validateUsr('asddsa'), true);
+// Test.assertEquals(validateUsr('a'), false);
+// Test.assertEquals(validateUsr('Hass'), false);
+// Test.assertEquals(validateUsr('Hasd_12assssssasasasasasaasasasasas'), false);
+// Test.assertEquals(validateUsr(''), false);
+// Test.assertEquals(validateUsr('____'), true);
+// Test.assertEquals(validateUsr('012'), false);
+// Test.assertEquals(validateUsr('p1pp1'), true);
+// Test.assertEquals(validateUsr('asd43 34'), false);
+// Test.assertEquals(validateUsr('asd43_34'), true);
+// })
+
